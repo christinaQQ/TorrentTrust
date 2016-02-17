8291547a9045bc19ebf194869db13210038d3fcb
David Xu <dxu2012@gmail.com>
2016-02-17 18:33:15 -0500

    Add package-info.java to tomp2pcompat package

---
+++ cweb/distributed-core/src/main/java/moe/cdn/cweb/dht/internal/tomp2pcompat/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * /**
+ * Contains wrappers around tomp2p internal classes. This refactors tomp2p to use futures in a
+ * sane way (namely, {@link com.google.common.util.concurrent.ListenableFuture}).
+ */
+package moe.cdn.cweb.dht.internal.tomp2pcompat;
