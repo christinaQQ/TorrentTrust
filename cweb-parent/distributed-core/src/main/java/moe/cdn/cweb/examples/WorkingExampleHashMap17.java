2ef67ffaa751edf8751f8e59dc390684c92d1ac9
Jim Chen <knh.jabbany@gmail.com>
2016-03-23 20:21:06 -0400

    Remove badly implemented SignatureUtils from examples package.
    WEHM17 should be using the common SignatureUtils and KeyUtils instead of an old example version.

--- cweb-parent/distributed-core/src/main/java/moe/cdn/cweb/examples/WorkingExampleHashMap17.java
+++ cweb-parent/distributed-core/src/main/java/moe/cdn/cweb/examples/WorkingExampleHashMap17.java
@@ -1,22 +1,5 @@
 package moe.cdn.cweb.examples;
 
-import com.google.common.util.concurrent.Futures;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Key;
-import com.google.inject.TypeLiteral;
-import com.google.inject.grapher.graphviz.GraphvizGrapher;
-import com.google.inject.grapher.graphviz.GraphvizModule;
-import moe.cdn.cweb.TorrentTrustProtos;
-import moe.cdn.cweb.TorrentTrustProtos.SignedUser;
-import moe.cdn.cweb.dht.CwebMap;
-import moe.cdn.cweb.dht.internal.PeerDhtShutdownable;
-import net.tomp2p.dht.PeerDHT;
-import net.tomp2p.futures.FutureBootstrap;
-import net.tomp2p.p2p.Peer;
-import net.tomp2p.peers.PeerAddress;
-import net.tomp2p.replication.IndirectReplication;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
@@ -25,18 +8,49 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Future;
-import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+import com.google.inject.grapher.graphviz.GraphvizGrapher;
+import com.google.inject.grapher.graphviz.GraphvizModule;
+
+import moe.cdn.cweb.SecurityProtos.KeyPair;
+import moe.cdn.cweb.TorrentTrustProtos.SignedUser;
+import moe.cdn.cweb.TorrentTrustProtos.User;
+import moe.cdn.cweb.dht.CwebMap;
+import moe.cdn.cweb.dht.internal.PeerDhtShutdownable;
+import moe.cdn.cweb.security.utils.KeyUtils;
+import moe.cdn.cweb.security.utils.SignatureUtils;
+import net.tomp2p.dht.PeerDHT;
+import net.tomp2p.futures.FutureBootstrap;
+import net.tomp2p.peers.PeerAddress;
+import net.tomp2p.replication.IndirectReplication;
 
 public class WorkingExampleHashMap17 {
     // TODO: http://lists.tomp2p.net/pipermail/users/2013-July/000266.html
     private static final int PORT = 1717;
-    private static final TorrentTrustProtos.SignedUser USER17 =
-            SignatureUtils.buildSignedUserRecord(SignatureUtils.generateKeypair(), "17");
-    private static final TorrentTrustProtos.SignedUser USER18 =
-            SignatureUtils.buildSignedUserRecord(SignatureUtils.generateKeypair(), "18");
-    private static final TorrentTrustProtos.SignedUser USER19 =
-            SignatureUtils.buildSignedUserRecord(SignatureUtils.generateKeypair(), "19");
+
+    private static final KeyPair USER17_KEYS = KeyUtils.generateKeyPair();
+    private static final KeyPair USER18_KEYS = KeyUtils.generateKeyPair();
+    private static final KeyPair USER19_KEYS = KeyUtils.generateKeyPair();
+
+    private static final User USER17 =
+            User.newBuilder().setPublicKey(USER17_KEYS.getPublicKey()).setHandle("User 17").build();
+    private static final User USER18 =
+            User.newBuilder().setPublicKey(USER18_KEYS.getPublicKey()).setHandle("User 17").build();
+    private static final User USER19 =
+            User.newBuilder().setPublicKey(USER19_KEYS.getPublicKey()).setHandle("User 17").build();
+
+    private static final SignedUser USER17_SIGNED = SignedUser.newBuilder()
+            .setSignature(SignatureUtils.signMessage(USER17_KEYS, USER17)).setUser(USER17).build();
+    private static final SignedUser USER18_SIGNED = SignedUser.newBuilder()
+            .setSignature(SignatureUtils.signMessage(USER18_KEYS, USER18)).setUser(USER18).build();
+    private static final SignedUser USER19_SIGNED = SignedUser.newBuilder()
+            .setSignature(SignatureUtils.signMessage(USER19_KEYS, USER19)).setUser(USER19).build();
 
     static PeerDhtShutdownable[] createPeers(Injector[] injectors) throws IOException {
         PeerDhtShutdownable[] peers = new PeerDhtShutdownable[injectors.length];
@@ -45,9 +59,11 @@ public class WorkingExampleHashMap17 {
             new IndirectReplication(peers[i].getDhtInstance()).replicationFactor(5).start();
         }
         return peers;
-        // Originally, peer[0] was the master peer and all others connected to it
-        //    peers[i] = new PeerBuilderDHT(new PeerBuilder(Number160s.fromCwebId(RND))
-        //            .masterPeer(peers[0].peer()).start()).start();
+        // Originally, peer[0] was the master peer and all others connected to
+        // it
+        // peers[i] = new PeerBuilderDHT(new
+        // PeerBuilder(Number160s.fromCwebId(RND))
+        // .masterPeer(peers[0].peer()).start()).start();
     }
 
 
@@ -59,10 +75,8 @@ public class WorkingExampleHashMap17 {
     static void bootstrap(PeerDhtShutdownable[] peers) {
         // tell all peers about each other starting from master at 0
         // TODO replace
-        Collection<PeerAddress> all = Arrays.stream(peers)
-                .map(PeerDhtShutdownable::getDhtInstance)
-                .map(PeerDHT::peerAddress)
-                .collect(Collectors.toList());
+        Collection<PeerAddress> all = Arrays.stream(peers).map(PeerDhtShutdownable::getDhtInstance)
+                .map(PeerDHT::peerAddress).collect(Collectors.toList());
         List<FutureBootstrap> allFutures = Arrays.stream(peers)
                 .map(p -> p.getDhtInstance().peer().bootstrap().bootstrapTo(all).start())
                 .collect(Collectors.toList());
@@ -84,10 +98,10 @@ public class WorkingExampleHashMap17 {
         }
 
 
-//        PeerDHT master = null;
+        // PeerDHT master = null;
         try {
             PeerDhtShutdownable[] peers = createPeers(injectors);
-//            master = peers[0];
+            // master = peers[0];
 
             bootstrap(peers);
 
@@ -95,50 +109,44 @@ public class WorkingExampleHashMap17 {
             Injector injector2 = injectors[17];
             Injector injector3 = injectors[23];
 
-            CwebMap<SignedUser> map1 = injector1.getInstance(Key.get(
-                    new TypeLiteral<CwebMap<SignedUser>>() {
-                    }));
-            CwebMap<SignedUser> map2 = injector2.getInstance(Key.get(
-                    new TypeLiteral<CwebMap<SignedUser>>() {
-                    }));
-            CwebMap<SignedUser> map3 = injector3.getInstance(Key.get(
-                    new TypeLiteral<CwebMap<SignedUser>>() {
-                    }));
+            CwebMap<SignedUser> map1 =
+                    injector1.getInstance(Key.get(new TypeLiteral<CwebMap<SignedUser>>() {}));
+            CwebMap<SignedUser> map2 =
+                    injector2.getInstance(Key.get(new TypeLiteral<CwebMap<SignedUser>>() {}));
+            CwebMap<SignedUser> map3 =
+                    injector3.getInstance(Key.get(new TypeLiteral<CwebMap<SignedUser>>() {}));
 
 
-            boolean r1 = map1.put(USER17.getUser().getPublicKey().getHash(), USER17).get();
-            boolean r2 = map2.put(USER18.getUser().getPublicKey().getHash(), USER18).get();
-            boolean r3 = map3.put(USER19.getUser().getPublicKey().getHash(), USER19).get();
+            boolean r1 = map1.put(USER17_KEYS.getPublicKey().getHash(), USER17_SIGNED).get();
+            boolean r2 = map2.put(USER18_KEYS.getPublicKey().getHash(), USER18_SIGNED).get();
+            boolean r3 = map3.put(USER19_KEYS.getPublicKey().getHash(), USER19_SIGNED).get();
+            System.out.println("Put results " + r1 + "," + r2 + "," + r3);
 
 
             // insert some data and wait
-            List<Boolean> putResults = Futures.allAsList(
-                    map1.put(USER17.getUser().getPublicKey().getHash(), USER17),
-                    map2.put(USER18.getUser().getPublicKey().getHash(), USER18),
-                    map3.put(USER19.getUser().getPublicKey().getHash(), USER19)).get();
+            @SuppressWarnings("unchecked")
+            List<Boolean> putResults =
+                    Futures.allAsList(map1.put(USER17_KEYS.getPublicKey().getHash(), USER17_SIGNED),
+                            map2.put(USER18_KEYS.getPublicKey().getHash(), USER18_SIGNED),
+                            map3.put(USER19_KEYS.getPublicKey().getHash(), USER19_SIGNED)).get();
             if (putResults.stream().allMatch(Boolean::booleanValue)) {
                 System.out.println("Seeded.");
             } else {
                 System.out.println("Failed to seed.");
             }
 
-            Future<SignedUser> one = map3.get(USER18.getUser().getPublicKey().getHash());
+            Future<SignedUser> one = map3.get(USER18_KEYS.getPublicKey().getHash());
             System.out.println("getOne() ==> " + one.get());
 
-            Future<Collection<SignedUser>> all =
-                    map3.all(USER18.getUser().getPublicKey().getHash());
+            Future<Collection<SignedUser>> all = map3.all(USER18_KEYS.getPublicKey().getHash());
             System.out.println("getAll() ==> " + all.get());
 
             System.out.println("Shutting down...");
-            Futures.allAsList(Arrays.stream(peers)
-                    .map(PeerDhtShutdownable::shutdown)
+            Futures.allAsList(Arrays.stream(peers).map(PeerDhtShutdownable::shutdown)
                     .collect(Collectors.toList())).get();
             System.out.println("Done.");
 
         } finally {
-//            if (master != null) {
-//                master.shutdown();
-//            }
         }
     }
 
